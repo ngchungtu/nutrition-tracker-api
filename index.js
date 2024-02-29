498c8155beb07e0870d8ced5e4db8d87001a7695
Chg Tu - 120837 <nguyentutsq@gmail.com>
2024-02-29 09:42:43 +0700

    clean code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import * as err_types from './errLog/index.js'
@@ -11,12 +11,10 @@ const app = express();
 app.use(cors())
 
 const PORT = process.env.PORT
-const URI = process.env.MONGO
 
 // app.use(express.json({ limit: '50mb' }))
 // app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
-connectToMongo()
 app.listen(PORT, () => {
     try {
         connectToMongo()
@@ -25,18 +23,6 @@ app.listen(PORT, () => {
         console.log(err_types.errLog[500]);
     }
 });
-
-// mongoose
-//     .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => {
-//         console.log('Connected to DB');
-//         app.listen(PORT, () => {
-//             console.log(`Server is running on port ${PORT}`);
-//         });
-//     })
-//     .catch((err) => {
-//         console.log('err', err);
-//     });
 
 app.get("/foods", async (req, res) => {
     try {
