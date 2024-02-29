3f3f2a1874c3c2b721f20fa5371e163749aadefd
Chg Tu - 120837 <nguyentutsq@gmail.com>
2024-02-29 09:29:05 +0700

    commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-// import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import * as err_types from './errLog/index.js'
@@ -10,30 +10,33 @@ dotenv.config()
 const app = express();
 app.use(cors())
 
-// const API_URL = process.env.MONGO
 const PORT = process.env.PORT
+const URI = process.env.MONGO
 
-app.use(express.json({ limit: '50mb' }))
-app.use(express.urlencoded({ extended: true, limit: '50mb' }))
-/* #region  func connection to MongoDB */
-// const connectToMongo = async () => {
-//     try {
-//         await mongoose.connect(API_URL)
-//         console.log('Connect Successfully!');
-//     } catch (error) {
-//         console.log(err_types.errLog[404]);
-//     }
-// }
-/* #endregion */
+// app.use(express.json({ limit: '50mb' }))
+// app.use(express.urlencoded({ extended: true, limit: '50mb' }))
+
 connectToMongo()
-// app.listen(PORT, () => {
-//     try {
-//         connectToMongo()
-//         console.log(`Connect to DB successfully, port: ${PORT}`);
-//     } catch (error) {
-//         console.log(err_types.errLog[500]);
-//     }
-// });
+app.listen(PORT, () => {
+    try {
+        connectToMongo()
+        console.log(`Connect to DB successfully, port: ${PORT}`);
+    } catch (error) {
+        console.log(err_types.errLog[500]);
+    }
+});
+
+// mongoose
+//     .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => {
+//         console.log('Connected to DB');
+//         app.listen(PORT, () => {
+//             console.log(`Server is running on port ${PORT}`);
+//         });
+//     })
+//     .catch((err) => {
+//         console.log('err', err);
+//     });
 
 app.get("/foods", async (req, res) => {
     try {
